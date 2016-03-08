f1a2d89b2c12e4235026432f8c9e98cca1604523
Alexander-Andrade <veimar.94@gmail.com>
2016-03-08 12:25:12 +0300

    init

---
+++ py_udp_chat/py_udp_chat.py
@@ -0,0 +1,46 @@
+from socket import*
+import sys
+import struct
+
+class Peer:
+
+    def __init__(self,group,port):
+        self.nickname = ''
+        self.port = int(port)
+        self.group = group
+        #for joininig and unjoining to the group
+        self.mreq = b''
+        self.net_interface = getaddrinfo(gethostname(),'http',AF_INET)[0][4][0]
+        self.sock = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)
+        self.__join_group(self.sock)
+        self.sock.bind((self.net_interface,port))
+        #join group
+       
+
+    def __join_group(self,sock):
+        self.mreq = inet_aton(self.group) + inet_aton(self.net_interface)
+        sock.setsockopt(SOL_IP,IP_ADD_MEMBERSHIP,self.mreq)
+        sock.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+        #default
+        #self.sock.setsockopt(IPPROTO_IP,IP_MULTICAST_TTL,struct.pack('B',1))
+
+    def __unjoin_group(self,sock):
+        self.sock.setsockopt(SOL_IP,IP_DROP_MEMBERSHIP,self.mreq)
+
+    def group_send(self,msg):
+        self.sock.sendto(msg,0,(self.group,self.port))
+
+    def send(self,msg,addr):
+        self.sock.sendto(msg,0,addr)
+
+    def recv(self,num):
+        return self.sock.recvfrom(num)
+
+    def __del__(self):
+        self.__unjoin_group(self.sock)
+        self.sock.close()
+
+if __name__=='__main__':
+    peer = Peer('224.0.0.1',6000)
+    print(peer.recv(1024))
+    del peer
